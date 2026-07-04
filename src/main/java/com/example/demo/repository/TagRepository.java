b9b849bce13ee46bb8d41b38c60c439d3c2b4f7c
Higa Kosk <higakosk@gmail.com>
2026-07-04 10:12:58 +0900

    Edit: make trends to show only poppular one within 3 days.

--- src/main/java/com/example/demo/repository/TagRepository.java
+++ src/main/java/com/example/demo/repository/TagRepository.java
@@ -12,6 +12,12 @@ public interface TagRepository extends JpaRepository<Tag, Long>{
     Optional<Tag> findByName(String name);
 
     // 中間テーブル（post_tags）での登場回数が多い順に、上位5件のタグを抜き出すSQL
-    @Query(value = "SELECT t.* FROM tags t LEFT JOIN post_tags pt ON t.id = pt.tag_id GROUP BY t.id ORDER BY COUNT(pt.post_id) DESC LIMIT 5", nativeQuery = true)
+    @Query(value = "SELECT t.* FROM tags t " +
+               "LEFT JOIN post_tags pt ON t.id = pt.tag_id " +
+               "LEFT JOIN posts p ON pt.post_id = p.id " +
+               "WHERE p.created_at >= DATEADD('DAY', -3, CURRENT_TIMESTAMP) " + // 💡 '3 DAYS' とシングルクォーテーションで囲む
+               "GROUP BY t.id " +
+               "ORDER BY COUNT(pt.post_id) DESC " +
+               "LIMIT 5", nativeQuery = true)
     List<Tag> findTop5Trends();
 }
